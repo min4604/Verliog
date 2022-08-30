8522531c616aec22c6beed1cc7a72a690da84f5d
min4604 <mina4604sh@gmail.com>
2022-08-30 20:49:21 +0800

    updata

---
+++ accomdemy_rv32i/RV32I/RV32I.srcs/sim_1/new/cpu_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns/1ps
+
+module cpu_tb;
+    reg        clk;
+    reg        reset;
+
+    cpu cpu0 (
+        .clk(clk),
+        .reset(reset)
+    );
+
+    initial begin
+        clk     = 0;
+        
+        reset   = 0;
+        #5 reset = 1; 
+        
+        forever #1 clk = ~clk; 
+       
+    end
+    initial begin
+        #60 $stop;
+        #1 $finish;
+    end
+      
+endmodule
--- accomdemy_rv32i/RV32I/RV32I.srcs/sources_1/new/decoder.v
+++ accomdemy_rv32i/RV32I/RV32I.srcs/sources_1/new/decoder.v
@@ -130,8 +130,8 @@ module decoder(
             /*
              * ========================================================
              */
-            7'b1101111:   begin             // J-Type
-                ra1         = 5'b0;  // rs1 not implied
+            7'b1101111:   begin             // J-Type : JAL
+                ra1         = 5'b0;         // rs1 not implied
                 ra2         = 5'b0;         // rs2 not implied
                 wa          = prog[11:7];   // rd implied
                 imm         = { {11{prog[31]}}, prog[31], prog[19:12], prog[20], prog[30:21], 1'b0 };  // imm implied
@@ -143,8 +143,50 @@ module decoder(
                 jmpe        = _enable;      // use jmp on PC
                 op          = 8'h1;         // data1 + data2
             end
-            default: 
-            begin
+
+            7'b1100111: begin               // I-Type : JALR
+                ra1         = prog[19:15];  // rs1 implied
+                ra2         = 5'b0;         // rs2 not implied
+                wa          = prog[11:7];   // rd implied
+                imm         = { {20{prog[31]}}, prog[31:20] };  // imm implied
+                re1         = _enable;      // rs1 used
+                re2         = _disable;     // rs2 not used
+                we          = _enable;      // rd required
+                pce         = _disable;     // use pc on ALU-data1
+                imme        = _enable;      // use imm on ALU-data2
+                jmpe        = _enable;      // use jmp on PC
+                op          = 8'h1;         // data1 + data2
+            end
+
+            7'b0110111: begin               // U-Type : LUI
+                ra1         = 5'b0;         // rs1 not implied, but forced to use X0
+                ra2         = 5'b0;         // rs2 not implied
+                wa          = prog[11:7];   // rd implied
+                imm         = { prog[31:12], 12'b0 };  // imm implied
+                re1         = _enable;      // forced to use X0, expecting 32'b0
+                re2         = _disable;     // rs2 not used
+                we          = _enable;      // rd required
+                pce         = _disable;     // expecting 32'b0 on ALU-data1
+                imme        = _enable;      // use imm on ALU-data2
+                jmpe        = _disable;      // use pc+4 on PC
+                op          = 8'h1;         // data1 + data2
+            end
+
+            7'b0010111: begin               // U-Type : AUIPC
+                ra1         = 5'b0;         // rs1 not implied
+                ra2         = 5'b0;         // rs2 not implied
+                wa          = prog[11:7];   // rd implied
+                imm         = { prog[31:12], 12'b0 };  // imm implied
+                re1         = _disable;     // rs1 not used
+                re2         = _disable;     // rs2 not used
+                we          = _enable;      // rd required
+                pce         = _enable;      // use pc on ALU-data1
+                imme        = _enable;      // use imm on ALU-data2
+                jmpe        = _disable;     // use pc+4 on PC
+                op          = 8'h1;         // data1 + data2
+            end
+
+            default: begin
                 ra1         = 5'b0;         // rs1 not implied
                 ra2         = 5'b0;         // rs2 not implied
                 wa          = 5'b0;         // rd not implied
