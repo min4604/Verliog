676285183d849c20bd40c32fb395463d7a9e5b5d
min4604 <mina4604sh@gmail.com>
2022-09-01 21:03:38 +0800

    updata

---
+++ RV32I/RV32I.srcs/sources_1/new/data_memory.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/01/2022 09:02:57 PM
+// Design Name: 
+// Module Name: data_memory
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module data_memory(
+    input en
+    );
+endmodule
